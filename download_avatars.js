61f1e105b5519863b713ede1818fa8424b320280
RayCJ87 <shihcr.ke@gmail.com>
2018-12-03 23:22:37 +0000

    print avatar step 7

--- download_avatars.js
+++ download_avatars.js
@@ -4,9 +4,27 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
   // ...
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request'
+    }
+  };
+
+  request(options, function(err, res, body) {
+    // console.log(res);
+    cb(err, body)
+    // console.log(body);
+    var data = JSON.parse(body);
+    for (var i = 0; i < data.length; i++) {
+        console.log(" avatar url: " + data[i]["avatar_url"]);
+
+    }
+
+  });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
+  // console.log("Errors:", err);
+  // console.log("Result:", result);
 });
