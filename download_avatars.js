5c733eda211a9087e6e9074b64389398909463f9
RayCJ87 <shihcr.ke@gmail.com>
2018-12-03 23:39:17 +0000

    step 8 finish

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var fs = require('fs');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -12,19 +13,29 @@ function getRepoContributors(repoOwner, repoName, cb) {
   };
 
   request(options, function(err, res, body) {
-    // console.log(res);
     cb(err, body)
-    // console.log(body);
     var data = JSON.parse(body);
     for (var i = 0; i < data.length; i++) {
-        console.log(" avatar url: " + data[i]["avatar_url"]);
-
+        var userID = data[i]['login'];
+        downloadImageByURL(data[i]["avatar_url"], userID);
     }
 
   });
 }
 
+function downloadImageByURL(url, filePath){
+  var fileStore =  './avatars/'+ filePath;
+  request.get(url)
+    .on('error', function(err){
+      throw err;
+    })
+    .on('response', function (response){
+      console.log('Downloading images of ' + filePath + '...');
+    })
+    .pipe(fs.createWriteStream(fileStore));
+}
+
 getRepoContributors("jquery", "jquery", function(err, result) {
-  // console.log("Errors:", err);
-  // console.log("Result:", result);
+  console.log("Errors:", err);
+  console.log("Result:", result);
 });
